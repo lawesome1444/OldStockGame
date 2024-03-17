eff49fc979befdfd23e8bfa2dd1143fd29102720
Conor <pacman1444@gmail.com>
2024-03-17 22:43:17 +0000

    Significantly improved wall kicking. Added a Debug UI for speed and rotation

--- Scripts/Player_Control.gd
+++ Scripts/Player_Control.gd
@@ -4,19 +4,29 @@ extends CharacterBody3D
 const SPEED = 6.25
 const JUMP_VELOCITY = 4.5
 var jumping
-var walltest
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 ## Create a camera variable
 var camera
 
+
+#Import UI Elements
+var xSpeed
+var ySpeed
+var zSpeed
+var rotationgui
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Import camera
 	camera = get_node("./OrbitCamera")
+	#Import UI
+	xSpeed = get_node("../../GUI/xSpeed")
+	ySpeed = get_node("../../GUI/ySpeed")
+	zSpeed = get_node("../../GUI/zSpeed")
+	rotationgui = get_node("../../GUI/Rotation")
 	
-	walltest = get_node("../../LevelGeometry/WallKickTest")
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -50,12 +60,11 @@ func _physics_process(delta):
 		
 		# Handle Wall Kicks
 	if Input.is_action_just_pressed("jump") and is_on_wall() and not is_on_floor():
+		# Give the player some height
 		velocity.y = 5
-		velocity.x = direction.x * -7.5
-		velocity.z = direction.z * -7.5
-		var playerCor = Vector2(self.position.x, self.position.z)
-		var wallcor = Vector2(walltest.global_position.x, walltest.global_position.z)
-		print(playerCor.angle_to_point(wallcor))
+		# Then bounce them off the wall depending on what side of the wall they are facing
+		velocity.x = get_wall_normal().x * 7.5
+		velocity.z = get_wall_normal().z * 7.5
 	
 	## Handle Acceleration and Deceleration
 	if direction:
@@ -64,6 +73,12 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, decelerate_player() * delta)
 		velocity.z = move_toward(velocity.z, 0, decelerate_player() * delta)
+	
+	
+	xSpeed.text = "X Speed = " + str(velocity.x)
+	ySpeed.text = "Y Speed = " + str(velocity.y)
+	zSpeed.text = "Z Speed = " + str(velocity.z)
+	rotationgui.text = "Rotation = " + str(rotation.y)
 	
 	
 	move_and_slide()
